3691bb253eca6fa59323ebce49eb39bba923d11f
Krish Bharadwaj <krishbharadwaj50@gmail.com>
2025-04-08 17:30:13 +0530

    Resume Added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Hero.css"
 import Profile from "../../assets/Hero.png"
+import Resume from "../../assets/KrishResume.pdf"
 
 const Hero = () => {
   return (
@@ -11,7 +12,9 @@ const Hero = () => {
           <p>I am full stack developer from Uttar Pradesh, India with 2 years of experience in companies like Cloud Quest.</p>
           <div className='hero-action'>
             <div className="hero-connect">Connect with me</div>
-            <div className="hero-resume">My resume</div>
+            <div className="hero-resume">
+              <a href={Resume} target='_blank'>My resume</a>
+            </div>
           </div>
       </div>
     </>
