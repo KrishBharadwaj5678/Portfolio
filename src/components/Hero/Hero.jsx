d42c59c955f3003d09d7748951258faeb961a020
Krish Bharadwaj <krishbharadwaj50@gmail.com>
2025-04-08 18:32:58 +0530

    Files Updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./Hero.css"
 import Profile from "../../assets/Hero.png"
 import Resume from "../../assets/KrishResume.pdf"
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Hero = () => {
   return (
@@ -11,7 +12,11 @@ const Hero = () => {
           <h1><span>I'm Krish</span>, full stack developer based in India.</h1>
           <p>I am full stack developer from Uttar Pradesh, India with 2 years of experience in companies like Cloud Quest.</p>
           <div className='hero-action'>
-            <div className="hero-connect">Connect with me</div>
+              <AnchorLink className="anchor-link" offset={20} href="#contact">
+                <div className="hero-connect">
+                    Connect with me
+                </div>
+              </AnchorLink>
             <div className="hero-resume">
               <a href={Resume} target='_blank'>My resume</a>
             </div>
